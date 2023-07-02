63b55efd6910fc13f44b73ad7b1d0f3c8337c1b3
inna <99030586+Journey5873@users.noreply.github.com>
2023-07-02 21:29:06 +0900

    Time: 55 ms (60.44%), Space: 14 MB (99.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        # make a set
+        numSet =  set()
+        
+        # make output list
+        output = []
+
+        # check the nums list
+        for i in range(len(nums)):
+            num = target - nums[i]
+            if num in numSet:
+                output.append(i)
+                output.append(nums.index(num))
+                return output
+            numSet.add(nums[i]) 
+            
+
+            
+            
+
+        
+
