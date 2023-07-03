9da3d3f59737e117da63f6b8deff9b6f1c544d61
inna <99030586+Journey5873@users.noreply.github.com>
2023-07-03 21:54:20 +0900

    Time: 1213 ms (17.63%), Space: 14.7 MB (39.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        index = count = 0
-        while count < len(nums):
-            if nums[index] == 0:
-                nums.append(0)
-                nums.pop(index)
-            else: 
-                index += 1
-            count += 1
+        for num in nums:
+            if num == 0:
+                nums.remove(num)
+                nums.append(num)
+        return nums
