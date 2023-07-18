be163688e19f08992c29b42a8b116973254f107d
inna <99030586+Journey5873@users.noreply.github.com>
2023-07-18 22:28:59 +0900

    Time: 16 ms (95.52%), Space: 13.1 MB (99.46%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        for i in range(0, len(nums1)-m):
+            nums1.pop()
+        nums1.extend(nums2)
+        nums1.sort()
