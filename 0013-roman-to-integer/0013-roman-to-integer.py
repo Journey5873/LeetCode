d45f886e8a0ec8df3c07592c36b79e56839f722c
inna <99030586+Journey5873@users.noreply.github.com>
2023-08-31 12:05:40 +0900

    Time: 33 ms (56.94%), Space: 13.3 MB (42.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def romanToInt(self, s):
         romanDict = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "": 0}
+        
         romanInt = 0
+        
         temp = ""
-        for i in range(len(s)-1, -1, -1):
+        i = len(s) - 1
+        while i >= 0 :
             roman = s[i]
             if romanDict[temp] > romanDict[roman]:
                 romanInt -= romanDict[roman]
             else:
                 romanInt += romanDict[roman]
             temp = roman
+            i -= 1
         return romanInt
