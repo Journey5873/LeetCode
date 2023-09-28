bf1df66b3a6e6e3d220c6fcf69eab8441e6cfb70
inna <99030586+Journey5873@users.noreply.github.com>
2023-09-28 13:57:16 +0900

    Time: 10 ms (87.84%), Space: 13 MB (99.39%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        digitsList = list(digits)
+        dic = {"":"", "2":"abc", "3":"def", "4":"ghi", "5": "jkl", 
+               "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        
+        if not digitsList:
+            return []
+        
+        result = []
+
+        def dfs(currentCombination, nextDigit):
+            if not nextDigit:
+                result.append(currentCombination)
+                return 
+            
+            currentDigit = nextDigit[0]
+            letters = dic[currentDigit]
+
+            for i in range(len(letters)):
+                dfs(currentCombination + letters[i], nextDigit[1:])
+
+        dfs("", digitsList)
+        
+        return result
