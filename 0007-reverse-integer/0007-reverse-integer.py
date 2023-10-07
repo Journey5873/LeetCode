0bac08bb685b5bee5a0d185832280896996259c1
inna <99030586+Journey5873@users.noreply.github.com>
2023-10-07 12:31:18 +0900

    Time: 22 ms (15.99%), Space: 13.2 MB (73.66%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,26 +1,26 @@
 class Solution(object):
     def reverse(self, x):
-        INT_MAX = 2**31-1
+        INT_MAX = 2**31 - 1
         INT_MIN = -2**31
         isNegative = False
-
-        if x < INT_MIN or x > INT_MAX:
-            return 0
 
         if x < 0:
             isNegative = True
             x = -x
 
-        ret = 0
-        while x :
+        reversedX = 0
+        while x:
             temp = x % 10
-            ret = ret * 10 + temp
-            x = x //10
+            reversedX = reversedX * 10 + temp
+            x = x // 10
         
         if isNegative:
-            ret = -ret
-
-        if ret < INT_MIN or ret > INT_MAX:
-            return 0
+            reversedX = -reversedX
         
-        return ret
+        if reversedX < INT_MIN or reversedX > INT_MAX:
+            return 0
+        return reversedX
+
+
+solution = Solution()
+print(solution.reverse(-123))
