6a3ba0d136aa171042f47a6010d43d47631fba46
inna <99030586+Journey5873@users.noreply.github.com>
2023-10-24 20:39:35 +0900

    Time: 52 ms (87.46%), Space: 14.2 MB (97.69%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,17 +1,24 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                first = last = mid
-                while last + 1 <= len(nums) - 1 and nums[last + 1] == target:
-                    last += 1
-                while first - 1 >= 0 and nums[first - 1] == target:
-                    first -= 1
-                return [first, last]
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return [-1, -1]
+        def lowerBound(left, right):
+            while left < right:
+                mid = (left+right)//2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return right
+        def uppperBound(left, right):
+            while left < right:
+                mid = (left+right)//2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return right - 1
+        
+        first = lowerBound(0, len(nums))
+        last = uppperBound(0, len(nums))
+        if first > last:
+            return[-1,-1]
+        return [first, last]
