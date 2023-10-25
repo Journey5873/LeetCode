8acbcc6f8b2d5bfcbef3035b122a0f24955b31ce
inna <99030586+Journey5873@users.noreply.github.com>
2023-10-25 09:47:36 +0900

    Time: 44 ms (61.47%), Space: 14.1 MB (47.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        # make a set to store complements
-        numSet =  set()
-
-        # check the nums list
+        diffDic = {}
         for i in range(len(nums)):
-            num = target - nums[i]
-            if num in numSet:
-                return [i, nums.index(num)]
-            numSet.add(nums[i]) 
-            
-        # no solution found
-        return []
-            
-
-            
-            
-
-        
-
+            diff = target - nums[i]
+            if diff in diffDic:
+                return [diffDic[diff], i]
+            diffDic[nums[i]] = i
