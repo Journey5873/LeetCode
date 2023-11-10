15c6e0cc495ddeacd976f80c12ac7220b6a99f04
inna <99030586+Journey5873@users.noreply.github.com>
2023-11-10 12:19:49 +0900

    Time: 7 ms (95.59%), Space: 13.3 MB (57.18%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,35 +1,27 @@
 class MyQueue():
-
     def __init__(self):
         self.inStack = []
         self.outStack = []
 
-    def push(self, x):
+    def push(self,  x):
         self.inStack.append(x)
-        return None
-
+    
     def pop(self):
         if not self.outStack:
-            i = 0
-            lenght = len(self.inStack)
-            while i in range (0, lenght):
-                value = self.inStack.pop()
-                self.outStack.append(value)
-                i += 1
+            for _ in range(len(self.inStack)):
+                self.outStack.append(self.inStack.pop())
         return self.outStack.pop()
-
+    
     def peek(self):
         if not self.outStack:
-            i = 0
-            lenght = len(self.inStack)
-            while i in range (0, lenght):
-                value = self.inStack.pop()
-                self.outStack.append(value)
-                i += 1
+            for _ in range(len(self.inStack)):
+                self.outStack.append(self.inStack.pop())
         return self.outStack[-1]
-
+    
     def empty(self):
-        if len(self.inStack) == 0 and len(self.outStack) == 0:
+        if not self.inStack and not self.outStack:
             return True
         else:
             return False
+    
+    
