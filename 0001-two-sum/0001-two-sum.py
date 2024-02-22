cde516620c5abab82f1254a1fc08fbade12aaebd
inna <99030586+Journey5873@users.noreply.github.com>
2024-02-22 14:18:48 +0900

    Time: 44 ms (63.73%), Space: 12.6 MB (58.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        diffDic = {}
+        dic = {}
+        
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in diffDic:
-                return [diffDic[diff], i]
-            diffDic[nums[i]] = i
+            diff = target-nums[i]
+            if diff in dic:
+                return [dic[diff], i]
+            dic[nums[i]] = i
+
         return []
