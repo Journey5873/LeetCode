30cdeef3cc9faa41277864cbe1ce88505c5d30d1
inna <99030586+Journey5873@users.noreply.github.com>
2024-02-29 19:11:05 +0900

    Time: 64 ms (56.92%), Space: 11.6 MB (97.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,31 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        
+
+        MAX_INT = 2**31 - 1
+        MIN_INT = -2**31
+
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
         start, end = 0, len(nums1)
 
         while start <= end:
-            partition1 = (start + end) // 2
-            partition2 = (len(nums1)+len(nums2)+1) // 2 - partition1
 
-            max_left_1 = nums1[partition1 - 1] if partition1 != 0 else float("-inf")
-            min_right_1 = nums1[partition1] if partition1 != len(nums1) else float("inf")
+            partition_x = (start + end) // 2
+            partition_y = (len(nums1) + len(nums2) + 1) // 2 - partition_x
 
-            max_left_2 = nums2[partition2-1] if partition2 != 0 else float("-inf")
-            min_right_2 = nums2[partition2] if partition2 != len(nums2) else float("inf")
+            max_left_x = nums1[partition_x - 1] if partition_x != 0 else MIN_INT
+            min_right_x = nums1[partition_x] if partition_x != len(nums1) else MAX_INT
 
-            if max_left_1 <= min_right_2 and max_left_2 <= min_right_1:
-                if (len(nums1)+len(nums2)) % 2 == 0:
-                    return (float(max(max_left_1, max_left_2)) + float(min(min_right_1, min_right_2))) / 2.0
+            max_left_y = nums2[partition_y - 1] if partition_y != 0 else MIN_INT
+            min_right_y = nums2[partition_y] if partition_y != len(nums2) else MAX_INT
+
+            if max_left_x <= min_right_y and max_left_y <= min_right_x:
+                if (len(nums1) + len(nums2)) % 2 == 0:
+                    return (max(max_left_x, max_left_y) + min(min_right_x, min_right_y))/ 2.0
                 else:
-                    return float(max(max_left_1, max_left_2) )
-            elif max_left_1 > min_right_2:
-                end = partition1 - 1
-            else:
-                start = partition1 + 1
+                    return max(max_left_x, max_left_y)
+            elif max_left_x > min_right_y: # moves toward left in nums1 array
+                end = partition_x - 1
+            else: # moves toward right in nums1 array
+                start = partition_x + 1
