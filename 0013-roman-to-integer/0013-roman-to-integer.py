120fe20e94fc36730b37da1664db28cc8fedb915
inna <99030586+Journey5873@users.noreply.github.com>
2024-03-01 12:53:52 +0900

    Time: 8 ms (99.94%), Space: 11.6 MB (91.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def romanToInt(self, s):
-        romanDict = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "": 0}
-        prev = ""
-        number = 0
-        for i in range(len(s)-1, -1, -1):
-            curr = s[i]
-            if romanDict[prev] > romanDict[curr]:
-                number -= romanDict[curr]
+        roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, 
+                     "C": 100, "D": 500, "M": 1000}
+        ret = 0
+        idx = len(s) - 1
+
+        prev = 2**31 - 1
+        for i in range(len(s)):
+
+            if prev < roman_dic[s[i]]:
+                ret += roman_dic[s[i]]
+                ret -= prev * 2
             else:
-                number += romanDict[curr]
-            prev = curr
-        return number
+                ret += roman_dic[s[i]]
+
+            prev = roman_dic[s[i]]
+
+        return ret
