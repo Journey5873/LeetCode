2e1b58ae8e131e52741be3aa0bbb57c93959800f
inna <99030586+Journey5873@users.noreply.github.com>
2024-03-01 13:58:42 +0900

    Time: 29 ms (6.59%), Space: 12 MB (53.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -8,13 +8,11 @@ class Solution(object):
 
         for i in range(1, len(strs)):
             current_word = strs[i]
-            shorter_word_len = min(len(current_word), len(prefix))
 
             j = 0
-            while j < shorter_word_len:
-                if prefix[j] != current_word[j]:
-                    break
+            while j < len(prefix) and j < len(current_word) and prefix[j] == current_word[j]:
                 j += 1
+
             prefix = prefix[:j]
             
             if not prefix:
