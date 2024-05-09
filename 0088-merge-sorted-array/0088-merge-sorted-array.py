bd6ceedbec7184fffd1fd688f8a1b00cbfacad73
inna <99030586+Journey5873@users.noreply.github.com>
2024-05-09 21:04:09 +0900

    Time: 22 ms (30.78%), Space: 11.5 MB (80.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        for i in range(0, len(nums1)-m):
+        for _ in range(len(nums1) - m):
             nums1.pop()
-        nums1.extend(nums2)
+        nums1 += nums2
         nums1.sort()
