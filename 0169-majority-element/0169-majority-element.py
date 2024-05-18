01be38db9312aab7b2d237153a738df0b388939b
inna <99030586+Journey5873@users.noreply.github.com>
2024-05-18 18:03:36 +0900

    Time: 160 ms (61.33%), Space: 13.5 MB (24.80%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,7 +2,6 @@ class Solution(object):
     def majorityElement(self, nums):
         majority_num = len(nums)//2
         num_dic = {}
-        majority = nums[0]
 
         for num in nums:
             if num in num_dic:
@@ -10,7 +9,6 @@ class Solution(object):
             else:
                 num_dic[num] = 1
             
-            if num_dic[num] >= majority_num:
-                if num_dic[majority] < num_dic[num]:
-                    majority = num
-        return majority
+            if num_dic[num] > majority_num:
+                return num
+        return 0
