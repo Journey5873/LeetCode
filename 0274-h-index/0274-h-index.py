0e39519a28007d91addf8724b85fa0d92a909b52
inna <99030586+Journey5873@users.noreply.github.com>
2024-07-14 20:20:45 +0900

    Time: 23 ms (37.91%), Space: 11.7 MB (88.47%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def hIndex(self, citations):
+        citations.sort()
+
+        for i in range(len(citations)):
+            if citations[i] >= len(citations) - i:
+                return len(citations) - i
+        return 0
